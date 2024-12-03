e0215a6a7d70513cc4828495b3978a4daf8c7515
Geunsik Lim <leemgs@users.noreply.github.com>
2024-12-03 15:15:47 +0900

    Add files via upload

---
+++ fake-usb-camera/emotion-flashing-face.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3 
+import pyfakewebcam
+import numpy as np
+import time
+import timeit
+
+from PIL import Image
+
+cam = pyfakewebcam.FakeWebcam('/dev/video20', 1280, 720)
+
+cam.print_capabilities()
+
+im0 = np.array( Image.open("singer-iu.jpg") )
+im1 = np.zeros((720,1280,3), dtype=np.uint8)
+
+while True:
+
+    t1 = timeit.default_timer() 
+    cam.schedule_frame(im0)
+    t2 = timeit.default_timer() 
+    print('write time:{}'.format(t2-t1))
+
+    time.sleep(1/60)
+
+    t1 = timeit.default_timer() 
+    cam.schedule_frame(im1)
+    t2 = timeit.default_timer() 
+    print('write time:{}'.format(t2-t1))
+    time.sleep(1/60)
